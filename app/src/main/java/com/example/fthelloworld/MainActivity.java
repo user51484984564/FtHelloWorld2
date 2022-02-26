40745bd83a4cc2b3b058926479f4e0146a1a355e
user51484984564 <edgaras.gaizauskas@gmail.com>
2022-02-26 13:56:57 +0200

    Added ChangeColor button and functionality

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fthelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tvMain;
     private  boolean isHelloWorld;
+    private boolean isColorChange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         this.tvMain = (TextView)findViewById(R.id.tvMain);
         this.isHelloWorld = true;
+        this.isColorChange = true;
     }
 
     public void onBtnMainClick(View view) {
@@ -29,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
             this.tvMain.setText(R.string.label_name);
         }
         this.isHelloWorld = !this.isHelloWorld;
+    }
+
+    public void onBtnMainChangeColor(View view) {
+        ColorStateList currentValue = this.tvMain.getTextColors();
+        if(this.isColorChange){
+            this.tvMain.setTextColor(Color.rgb(80,200, 80));
+        }
+        else {
+            this.tvMain.setTextColor(Color.rgb(0,0, 0));
+        }
+        this.isColorChange = !this.isColorChange;
     }
 }
