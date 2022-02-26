4d7960be781bc71285d74d416589d64a6738af0c
user51484984564 <edgaras.gaizauskas@gmail.com>
2022-02-26 13:37:32 +0200

    Added Main button

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.fthelloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMain;
+    private  boolean isHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = (TextView)findViewById(R.id.tvMain);
+        this.isHelloWorld = true;
+    }
+
+    public void onBtnMainClick(View view) {
+        String currentValue = this.tvMain.getText().toString();
+        if(this.isHelloWorld){
+            this.tvMain.setText("Somethin new");
+        }
+        else {
+            this.tvMain.setText(R.string.label_name);
+        }
+        this.isHelloWorld = !this.isHelloWorld;
     }
 }
